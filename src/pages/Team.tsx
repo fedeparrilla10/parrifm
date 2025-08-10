67a65af5739d022293fcaa6637ebff00ee5e4bbd
fedeparrilla10 <federicoivanpd@gmail.com>
2025-08-10 15:08:26 +0300

    feat: add logos & stadium images

--- src/pages/Team.tsx
+++ src/pages/Team.tsx
@@ -13,16 +13,31 @@ const Team: React.FC = () => {
   };
 
   return (
-    <section className="team">
-      <h1 className="team-title">{team?.name}</h1>
-      <h3>{team?.league}</h3>
-      <h4>{team?.country}</h4>
-      <p className="team-description">{team?.reason}</p>
+    <section className="flex flex-col items-center justify-center gap-1">
+      <div className="flex items-center justify-center">
+        <img src={team?.logo} alt={`${team?.name} logo`} className="w-1/2" />
+        {/* <img
+          src={team?.stadium.image}
+          alt={`${team?.stadium.name} stadium`}
+          className="team-stadium w-1/2"
+        /> */}
+      </div>
 
-      <Link to="/">
-        <button>Back</button>
-      </Link>
-      <button onClick={handleRandomize}>Random Team</button>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-2xl">{team?.name}</h1>
+        <h3>{team?.league}</h3>
+        <h4>{team?.country}</h4>
+        <p className="team-description">{team?.reason}</p>
+      </div>
+
+      <p>{team?.difficulty}</p>
+
+      <div className="flex gap-4 mt-8">
+        <Link to="/">
+          <button>Back</button>
+        </Link>
+        <button onClick={handleRandomize}>Random Team</button>
+      </div>
     </section>
   );
 };
--- src/utils/teamRecommendations.ts
+++ src/utils/teamRecommendations.ts
@@ -1,50 +1,94 @@
-export type TeamRecommendation = {
+interface TeamRecommendation {
   id: number;
   name: string;
+  logo: string;
   league: string;
   country: string;
   reason: string;
+  difficulty: number;
+  stadium: Stadium;
+}
+
+type Stadium = {
+  name: string;
+  image: string;
+  city: string;
 };
 
 export const teamRecommendations: TeamRecommendation[] = [
   {
     id: 1,
     name: "AFC Wimbledon",
+    logo: "/teams/logos/wimbledon.webp",
     league: "EFL League Two",
     country: "England",
     reason:
       "A fan-owned club with a remarkable rise through the leagues. Great for a long-term save.",
+    difficulty: 2,
+    stadium: {
+      name: "Plough Lane",
+      image: "/teams/stadiums/st_wimbledon.jpg",
+      city: "London",
+    },
   },
   {
     id: 2,
     name: "Union Berlin",
+    logo: "/teams/logos/union_berlin.webp",
     league: "Bundesliga",
     country: "Germany",
     reason:
       "Recently promoted and now competing in Europe. Offers a unique underdog challenge.",
+    difficulty: 4,
+    stadium: {
+      name: "Stadion An der Alten Försterei",
+      image: "/teams/stadiums/st_union_berlin.jpg",
+      city: "Berlin",
+    },
   },
   {
     id: 3,
     name: "Real Sociedad",
+    logo: "/teams/logos/rso.webp",
     league: "La Liga",
     country: "Spain",
     reason:
       "Known for youth development and attacking football. Ideal for building around young talent.",
+    difficulty: 3,
+    stadium: {
+      name: "Anoeta Stadium",
+      image: "/teams/stadiums/st_rso.jpg",
+      city: "San Sebastián",
+    },
   },
   {
     id: 4,
     name: "Atalanta",
+    logo: "/teams/logos/atalanta.webp",
     league: "Serie A",
     country: "Italy",
     reason:
       "Exciting tactics and a reputation for developing players. Perfect for tactical experimentation.",
+    difficulty: 4,
+    stadium: {
+      name: "Gewiss Stadium",
+      image: "/teams/stadiums/st_atalanta.webp",
+      city: "Bergamo",
+    },
   },
   {
     id: 5,
     name: "FC Nordsjælland",
+    logo: "/teams/logos/nordsjælland.webp",
     league: "Superliga",
     country: "Denmark",
     reason:
       "Focuses on youth and fast-paced football. Great for managers who love developing prospects.",
+    difficulty: 2,
+    stadium: {
+      name: "Right to Dream Park",
+      image: "/teams/stadiums/st_nordsjælland.jpg",
+      city: "Farum",
+    },
   },
 ];
