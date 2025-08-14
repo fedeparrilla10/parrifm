1839b1a78ba9e3012f3ce2cb31d74ae913ed6aee
fedeparrilla10 <federicoivanpd@gmail.com>
2025-08-14 15:32:06 +0300

    update: main routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,23 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home";
 import AllTeams from "./pages/AllTeams";
 import Team from "./pages/Team";
+import Selector from "./pages/Selector";
+import AISelector from "./pages/AISelector";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/teams" element={<AllTeams />} />
-          <Route path="/teams/:id" element={<Team />} />
-          {/* <Route path="/random_team" element={<div>Random Team Page</div>} /> */}
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="teams" element={<AllTeams />} />
+          <Route path="teams/:id" element={<Team />} />
+          <Route path="selector" element={<Selector />} />
+          <Route path="ai_selector" element={<AISelector />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
